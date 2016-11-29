8809b7fd43d68a93496f2c4f599ec1eceb07ec3b
Oskar Konstantyner <osk@templafy.com>
2016-11-29 15:52:54 +0100

    Test window.open

--- PowerPointWebAddIn1Web/Home.js
+++ PowerPointWebAddIn1Web/Home.js
@@ -20,15 +20,17 @@
 
     // Reads data from current document selection and displays a notification
     function getDataFromSelection() {
-        Office.context.document.getSelectedDataAsync(Office.CoercionType.Text,
-            function (result) {
-                if (result.status === Office.AsyncResultStatus.Succeeded) {
-                    showNotification('The selected text is:', '"' + result.value + '"');
-                } else {
-                    showNotification('Error:', result.error.message);
-                }
-            }
-        );
+        window.open("https://templafy.templafy.com/api-v1/Document/OpenDocument?temporaryFileName=2016-11-29T142715.2089635%2b0000-Base+template.pptx&documentName=Base+template.pptx&templateType=Presentation", "_blank");
+
+        //Office.context.document.getSelectedDataAsync(Office.CoercionType.Text,
+        //    function (result) {
+        //        if (result.status === Office.AsyncResultStatus.Succeeded) {
+        //            showNotification('The selected text is:', '"' + result.value + '"');
+        //        } else {
+        //            showNotification('Error:', result.error.message);
+        //        }
+        //    }
+        //);
     }
 
     // Helper function for displaying notifications
